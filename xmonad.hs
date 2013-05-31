4d4ef0dddf485b4547763c3b0ac6f6c06ac30603
blargg <anuibis@sbcglobal.net>
2013-05-30 23:33:33 -0500

    reduced to basic xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -1,81 +1,25 @@
 import XMonad
-import qualified XMonad.StackSet as W
-
-import XMonad.Actions.CycleWS
-
-import XMonad.Layout.Grid
-import XMonad.Layout.Tabbed
-import XMonad.Layout.NoBorders(smartBorders)
-
-import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeysP)
-import XMonad.Util.Scratchpad
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-import XMonad.Prompt
-import XMonad.Prompt.Shell(shellPrompt)
-import XMonad.Prompt.Window
-
-import System.IO(hPutStrLn)
-
--- Things that should always float
-myFloatHook = composeAll [
-	className =? "qemu" --> doFloat
-	]
-
-myLayoutHook = tiled ||| Mirror tiled ||| Grid ||| simpleTabbed
-	where
-		-- default tiling algorithm partitions the screen into two panes
-		tiled   = Tall nmaster delta ratio
-
-		-- The default number of windows in the master pane
-		nmaster = 1
-
-		-- Default proportion of screen occupied by master pane
-		ratio   = 1/2
-
-		-- Percent of screen to increment by when resizing panes
-		delta   = 3/100
-
-myBar = "xmobar"
-
---myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
---toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
---myConfig = defaultConfig { modMask = mod4Mask }
+modm = mod1Mask
 
 main = do
-	xmproc <- spawnPipe "~/bin/xmobar"
-	--xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
-	xmonad $ withUrgencyHook NoUrgencyHook defaultConfig
-			{ manageHook = manageDocks <+> myFloatHook <+> manageHook defaultConfig <+> scratchpadManageHook (W.RationalRect 0.25 0.25 0.5 0.5)
-			, layoutHook = avoidStruts $ smartBorders $ myLayoutHook
-			, logHook    = dynamicLogWithPP $ xmobarPP
-				{ ppOutput = hPutStrLn xmproc
-				, ppUrgent = xmobarColor "#CC0000" "" . wrap "**" "**"
-				, ppTitle  = xmobarColor "#8AE234" ""
-				}
-			, terminal = "urxvt"
-			}
-			`additionalKeysP`
-			[ ("M-p", shellPrompt defaultXPConfig { position = Top })
-			, ("M-S-a", windowPromptGoto defaultXPConfig { position = Top })
-			, ("M-a", windowPromptBring defaultXPConfig { position = Top })
-			, ("M-S-l", spawn "~/bin/lock")
-			, ("M-S-<Left>", shiftToPrev)
-			, ("M-S-<Right>", shiftToNext)
-			, ("M-<Up>", windows W.focusUp)
-			, ("M-S-<Up>", windows W.swapUp)
-			, ("M-<Down>", windows W.focusDown)
-			, ("M-S-<Down>", windows W.swapDown)
-			, ("M-`", toggleWS)
-			, ("M-s", moveTo Next EmptyWS)
-			, ("M-S-s", shiftTo Next EmptyWS)
-			, ("M-g", scratchpadSpawnAction defaultConfig { terminal = "urxvt" })
-			, ("<XF86AudioLowerVolume>", spawn "~/bin/volume down")
-			, ("<XF86AudioRaiseVolume>", spawn "~/bin/volume up")
-			, ("<XF86AudioMute>", spawn "~/bin/volume mute")
-			, ("M-b", sendMessage ToggleStruts)
-			]
+    xmproc <- spawnPipe "~/bin/xmobar"
+    xmonad $ defaultConfig
+        { terminal = "urxvt"
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , modMask = modm
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        } `additionalKeys`
+        [ ((modm .|. shiftMask, xK_l), spawn "~/bin/lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
