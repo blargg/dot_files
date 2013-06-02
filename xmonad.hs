2f8b386771c920a64a9d29a899f62355d0580c94
blargg <anuibis@sbcglobal.net>
2013-06-01 20:44:43 -0500

    Organized xmonad.hs, experimenting with layouts

--- xmonad.hs
+++ xmonad.hs
@@ -8,30 +8,69 @@ import XMonad.Actions.GridSelect
 import XMonad.Actions.CopyWindow
 import System.IO
 
-modm = mod1Mask
+import XMonad.Prompt
+import XMonad.Prompt.Shell
 
-myWorkspaces :: [WorkspaceId]
-myWorkspaces = zipWith (\num name -> num ++ ":" ++ name) (map show [1..]) ["general", "web", "programming", "music", "other"]
+import XMonad.Layout
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Accordion
+import XMonad.Layout.Circle
+import XMonad.Layout.StackTile
+import XMonad.Layout.WorkspaceDir
 
 main = do
     xmproc <- spawnPipe "~/bin/xmobar"
-    xmonad $ defaultConfig
-        { terminal = "urxvt"
-        , workspaces = myWorkspaces
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , modMask = modm
-        , logHook = dynamicLogWithPP xmobarPP
-            { ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "green" "" . shorten 50
-            }
-        } `additionalKeys` (
-        [ ((modm .|. shiftMask, xK_l), spawn "~/bin/lock")
-        , ((modm, xK_g), goToSelected defaultGSConfig)
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
-        ]
-        ++ [((m .|. modm, k), windows $ f i)
-            | (i,k) <- zip (myWorkspaces) [xK_1..]
-            , (f,m) <- [(W.view, 0), (W.shift, shiftMask), (copy, shiftMask .|. controlMask)]]
-        )
+    xmonad $ myConfig xmproc
+
+modm = mod1Mask
+
+myLayout =  setOptions $ tiled ||| Mirror Accordion ||| Circle ||| StackTile 1 (3/100) (1/2)
+    where setOptions = workspaceDir "~" . smartBorders . avoidStruts
+          tiled = Tall nmaster delta ratio
+          nmaster = 1
+          ratio = 1/2
+          delta = 3/100
+
+myWorkspaces :: [WorkspaceId]
+myWorkspaces = zipWith joinStr numbers names
+    where names = ["general", "web", "programming", "music", "other"]
+          numbers = (map show [1..])
+          joinStr = (\num name -> num ++ ":" ++ name)
+
+myConfig pipeproc = additionalKeys myConfig' myKeys
+    where myConfig' = defaultConfig
+                        { terminal = "urxvt"
+                        , workspaces = myWorkspaces
+                        , manageHook = manageDocks <+> manageHook defaultConfig
+                        , layoutHook = myLayout
+                        , modMask = modm
+                        , logHook = dynamicLogWithPP xmobarPP
+                            { ppOutput = hPutStrLn pipeproc
+                            , ppTitle = xmobarColor "green" "" . shorten 50
+                            }
+                        }
+
+myKeys = [ ((modm .|. shiftMask, xK_l), spawn "~/bin/lock")
+         , ((modm, xK_a), goToSelected defaultGSConfig)
+         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+         , ((0, xK_Print), spawn "scrot")
+         , ((modm .|. controlMask, xK_x), shellPrompt myXPConfig)
+         , ((modm .|. shiftMask, xK_a), changeDir myXPConfig)
+         ]
+         ++
+         [((m .|. modm, k), windows $ f i)
+         | (i,k) <- zip (myWorkspaces) [xK_1..]
+         , (f,m) <- [(W.view, 0), (W.shift, shiftMask), (copy, shiftMask .|. controlMask)]
+         ]
+
+myXPConfig = defaultXPConfig
+                { font = "-*-fixed-*-*-*-*-12-*-*-*-*-*-*-*"
+                , bgColor = "black"
+                , fgColor = "white"
+                , bgHLight = "white"
+                , fgHLight = "black"
+                , borderColor = "white"
+                , promptBorderWidth = 0
+                , position = Top
+                , height = 14
+                }
